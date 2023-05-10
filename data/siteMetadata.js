ddaab450c8a8cbcb98a7c72b022446c880199712
Oğulcan Girginç <ogulcan@girginc.com>
2023-05-10 20:52:14 +0300

    Update urls

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,8 +5,8 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://ogirginc.com',
+  siteRepo: 'https://github.com/ogirginc/ogirginc.com',
   // siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.jpg',
   socialBanner: '/static/images/twitter-card.png',
